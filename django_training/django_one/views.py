98308ecb90f0a9584ae9b567724361685f7c3571
Selvin-Dsilva <dsilvaselvinxavier@gmail.com>
2017-04-24 21:17:26 -0400

    progress after last lec

--- django_training/django_one/views.py
+++ django_training/django_one/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.template import loader
+from .models import Blog
+
 
 # Create your views here.
+
+def welcome(request):
+    data_blog=Blog.objects.all()
+    name="selvin"
+    template = loader.get_template('django_one/index.html')
+    context = {
+        'name': name,
+        'data_blog':data_blog
+    }
+    return render(request,'django_one/index.html',context)
+    #return HttpResponse(template.render(context, request))
+    #return render_to_response('index.html',{'name':name})
+    #return HttpResponse("Hello, world. You're at the polls index.")
+
+def train_view(request):
+    if request.method=="POST":
+        print("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
+        data=request.get("title")
+    else:
+        print("inside else")
+        data_blog=Blog.objects.all()
+        name="selvin"
+        context = {
+            'name': name,
+            'data_blog':data_blog
+        }
+        return render(request,'django_one/train.html',context)
